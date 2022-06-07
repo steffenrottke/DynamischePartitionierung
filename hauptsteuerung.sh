301a11b245ee41459c004cc11d364bbfcb83228f
michellejmueller <104498793+michellejmueller@users.noreply.github.com>
2022-06-07 14:26:12 +0200

    Upload hauptsteuerung
    Erweiterung für die gesamte Steuerung

---
+++ hauptsteuerung.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+clear
+#Benutzer wird aufgefordert, eine Gesamtspeichergroesse zu waehlen
+echo "Bitte geben Sie eine Gesamtspeichergroesse an."
+#Speichergroesse wird eingelesen
+read -r speichergroesse
+	#Nachdem die Groesse eingelesen wurde wird ueberprueft, ob die Eingabe andere Zeichen ausser Zahlen enthaelt oder nicht groesser als 0 ist
+	#Wenn das der Fall ist, wird solange nach der Groesse gefragt, bis keine anderen Zeichen ausser Zahlen enthalten sind und die Eingabe groesser 0 ist
+	#Als Quelle fuer die Funktionsaufrufe ist simulationssteuerungFunktionen.sh definiert
+	#Der Rueckgabewert der Funktion keineZeichenGroesserNull ist die Gesamtspeichergroesse, welche in der Variablen gewaehlteGesamtspeichergroesse gespeichert wird
+	source ./simulationssteuerungFunktionen.sh
+	while [ -n "$(printf '%s\n' "$speichergroesse" | sed 's/[0-9]//g')" ] || [[ ! $speichergroesse -gt 0 ]]
+	do
+		keineZeichenNichtGroesserNull speichergroesse
+		zeichen speichergroesse
+		read -r speichergroesse
+	done
+	gewaehlteGesamtspeichergroesse=$(keineZeichenGroesserNull)
+echo "Ihre gewaehlte Speichergroesse ist: $gewaehlteGesamtspeichergroesse"
+#Array mit der gewaehlten Eingabe zur Groesse
+array_gesamte_groesse=($gewaehlteGesamtspeichergroesse)
+
+
+echo ""
+echo ""
+#Benutzer wird aufgefordert, das Realisierungskonzept auszuwaehlen, es gibt die Wahl zwischen den Shortcuts f,b,n,w und r
+echo "Bitte geben Sie ein, welches Realisierungskonzept genutzt werden soll. Es stehen folgenden Realisierungskonzepte zur Verfuegung:
+-> f (First Fit)
+-> b (Best Fit)
+-> n (Next Fit)
+-> w (Worst Fit)
+-> r (Random)"
+#Das ausgewaehlte Konzept wird eingelesen
+read -r konzept
+	#Als Quelle wird die Datei simulationssteuerungFunktionen.sh angegeben
+	#Es wird ueberprueft, ob die Eingabe einem der Realisierungskonzepte entspricht oder ob erneut nach einer Eingabe gefragt wird
+	source ./simulationssteuerungFunktionen.sh
+	while ! { [ "$konzept" = "f" ] || [ "$konzept" = "b" ] || [ "$konzept" = "n" ] || [ "$konzept" = "w" ] || [ "$konzept" = "r" ]; }
+	do
+		ungueltigesRealisierungskonzept konzept
+		read -r konzept
+	done
+	#Die Eingabe entspricht einem der Realisierungskonzepte
+	#if-Abfragen fuer Realisierungskonzept zum ueberpruefen, welches Realisierungskonzept ausgewahlt wurde
+	#Die Funktionen werden aus der Quelle simulationssteuerungFunktionen.sh aufgerufen
+	source ./simulationssteuerungFunktionen.sh
+	#Es wurde das Konzept First Fit ausgewaehlt
+	if [ "$konzept" = "f" ]; then
+		echo "Ihr gewaehltes Konzept ist: First Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Wenn nicht der Befehl quit (q) gewaehlt wird der ausgewaehlte Befehl nach dem gewaehlten Realisierungskonzept ausgefuehrt (in diesem Fall First Fit)
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+                                abfrageAktion
+                                read -r befehl
+                                #./firstfit.sh
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #./unsetElement
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Best Fit ausgewaehlt
+	elif [ "$konzept" = "b" ]; then
+		echo "Ihr gewaehltes Konzept ist: Best Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+		#Wenn nicht der Befehl quit (q) gewaehlt wird der ausgewaehlte Befehl nach dem gewaehlten Realisierungskonzept ausgefuehrt (in diesem Fall Best Fit)
+                abfrageAktion
+                #Der Befehl wird eingelesen
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+                                abfrageAktion
+                                read -r befehl
+                                #./bestfit.sh
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #./unsetElement
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Next Fit ausgewaehlt
+	elif [ "$konzept" = "n" ]; then
+		echo "Ihr gewaehltes Konzept ist: Next Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+		#Wenn nicht der Befehl quit (q) gewaehlt wird der ausgewaehlte Befehl nach dem gewaehlten Realisierungskonzept ausgefuehrt (in diesem Fall Next Fit)
+                abfrageAktion
+                #Der Befehl wird eingelesen
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+                                abfrageAktion
+                                read -r befehl
+                                #./nextfit.sh
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #./unsetElement
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Worst Fit ausgewaehlt
+	elif [ "$konzept" = "w" ]; then
+		echo "Ihr gewaehltes Konzept ist: Worst Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+		#Wenn nicht der Befehl quit (q) gewaehlt wird der ausgewaehlte Befehl nach dem gewaehlten Realisierungskonzept ausgefuehrt (in diesem Fall Worst Fit)
+                abfrageAktion
+                #Der Befehl wird eingelesen
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+                                abfrageAktion
+                                read -r befehl
+                                #./worstfit.sh
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #./unsetElement
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Random ausgewaehlt
+	elif [ "$konzept" = "r" ]; then
+		echo "Ihr gewaehltes Konzept ist: Random."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+		#Wenn nicht der Befehl quit (q) gewaehlt wird der ausgewaehlte Befehl nach dem gewaehlten Realisierungskonzept ausgefuehrt (in diesem Fall Random)
+		abfrageAktion
+		#Der Befehl wird eingelesen
+       		read -r befehl
+                	while [ "$befehl" != "q" ]
+                	do
+                		ablaufRealisierungskonzept befehl
+				abfrageAktion
+				read -r befehl
+                		#./random.sh
+                	done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #./unsetElement
+                echo "Der Simulator wurde beendet."
+	fi
