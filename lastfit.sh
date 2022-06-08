7068f942a41f7b510ac2dcd804c2fb20ada77a69
michellejmueller <104498793+michellejmueller@users.noreply.github.com>
2022-06-08 17:54:50 +0200

    Upload lastfit.sh
    Angepasste Version von lastfit.sh zur hauptsteuerung.sh

--- lastfit.sh
+++ lastfit.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 function insertLastFit(){
-    seekName=$1 #der zu speichernde Name des Prozesses
-    seekGroesse=$2 #die zu speichernde Groesse des Prozesses
+    seekName=${array_name_groesse[0]} #der zu speichernde Name des Prozesses
+    seekGroesse=${array_name_groesse[1]} #die zu speichernde Groesse des Prozesses
     summe=0 #Hier wird die Summe der belegten Speicherblöcke hinterlegt
+    speicherplatz=$((${array_gesamte_groesse[0]})) #größe des Speichers
+
+	echo ""
+	echo ""
+	echo "Speicher:"
+	echo ${speicher[*]}
+	echo "--------"
+	echo "--------"
+	echo "Groesse:"
+	echo ${seekGroesse[*]}
+	echo "++++++++"
+	echo "++++++++"
+	echo "Name:"
+	echo ${seekName[*]}
+
     for i in ${groesse[@]} #Durch alle Array Inhalte vom Array groesse loopen
     do
 	let summe+=$i #Array Inhalte addieren um die derzeitige Füllung herauszufinden
     done
-    if [[ $((summe+$seekGroesse)) -gt $speicherplatz ]]; then #Wenn die Füllung+neue Zuweisung die Speicherkapazität überschreiten -> Abbrechen
+    if [[ $((summe+$(($seekGroesse)))) -gt $speicherplatz ]]; then #Wenn die Füllung+neue Zuweisung die Speicherkapazität überschreiten -> Abbrechen
 	return 0
     fi
     seekCounter=0 #Counter der die freien Plätze beinhaltet
@@ -16,9 +31,9 @@ function insertLastFit(){
     do
 	if [[ -z "${groesse[$i]}" ]]; then #Wenn die i-te Stelle im Speicher leer ist
 	    seekCounter=$((seekCounter+1)) #Aufeinander folgende leere Plätze werden hochgezählt
-	    if [[ $seekCounter -ge $seekGroesse ]]; then #Wenn der counter der leeren Plätze größer/gleich der unterzubnrGröße 
+	    if [[ $seekCounter -ge $(($seekGroesse)) ]]; then #Wenn der counter der leeren Plätze größer/gleich der unterzubnrGröße 
 		index=$(($i-($seekCounter-1)))
-		groesse[$index]=$seekGroesse #Größe an der Stelle im Array einfügen
+		groesse[$index]=$(($seekGroesse)) #Größe an der Stelle im Array einfügen
 		name[$index]=$seekName #Name an der Stelle im Array einfügen
 		return 1 #Erfolgreich -> Raus
 	    fi
@@ -26,8 +41,3 @@ function insertLastFit(){
     done
     return 0 #Nicht erfolgreich -> Ciao!
 }
-unset name
-unset groesse
-speicherplatz=10 #Gesamtgröße des Speichers
-
-#insertLastFit "HALLOWELT" 1 #How To Use
