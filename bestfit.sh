59e1e16d9fcd61f7ebd82ce7da454c65417fae32
michellejmueller <104498793+michellejmueller@users.noreply.github.com>
2022-06-16 23:21:36 +0200

    Upload bestfit und Steuerung
    Tests für Zusammenführung von Konzepten und Steuerung

---
+++ bestfit.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+function insertBestFit(){
+    speicherplatz=$1
+    seekName=$2
+    seekGroesse=$3
+    summe=0 #Hier wird die Summe der belegten Speicherblöcke hinterlegt
+
+    echo ">>>Das ist speicherplatz: $speicherplatz ;;; seekName: $seekName ;;; Das ist seekGroesse: $(($seekGroesse))"
+
+    for i in ${groesse[@]} #Durch alle Array Inhalte vom Array groesse loopen
+    do
+    let summe+=$i
+    done
+
+    if [[ $((summe+$(($seekGroesse)))) -gt $speicherplatz ]]; then #Wenn die Füllung+neue Zuweisung die Speicherkapazität überschreiten -> Abbrechen
+    echo "zu gross bei $seekName $(($seekGroesse))"
+    return 0
+    fi
+
+    seekCounter=0 #Counter der die freien Plätze beinhaltet
+    bereicheStelle=0
+    for (( i=0; i<$speicherplatz; i++ )) #Schleife in der Größe des Speicherplatzes
+    do
+        if [[ -z "${speicher[$i]}" ]]; then #Wenn die i-te Stelle im Speicher leer ist
+	    #echo "seekCounter: $seekCounter"
+            seekCounter=$((seekCounter+1)) #Aufeinander folgende leere Plätze werden hochgezählt
+    	    #seekCounter=$((seekCounter-1))
+	    #return 1 #Erfolgreich -> Raus
+        else
+	    if [[ $seekCounter -gt 0 ]]; then
+		    #echo ">>>Unterbrochen bei i: $i"
+        	    pStartCalc=$(($i-$seekCounter))
+		    #echo "pStartCalc: $pStartCalc"
+		    pEndCalc=$(($pStartCalc+$seekCounter-1))
+		    #echo "pEndClac: $pEndCalc"
+		    potentialStart+=($pStartCalc)
+		    #echo "potentialStart: ${potentialStart[*]}"
+		    potentialEnd+=($pEndCalc)
+		    #echo "potentialEnd: ${potentialEnd[*]}"
+		    #echo "----------"
+	            seekCounter=0
+	    fi
+        fi
+    done
+
+    if [[ $seekCounter -eq $speicherplatz ]]; then
+	potentialStart=(0)
+	potentialEnd=($speicherplatz)
+    fi
+
+
+    kleinsteDifferenz=$(($speicherplatz+1))
+    #echo "count arr: ${#potentialStart}"
+    for (( i=0; i<${#potentialStart}; i++ ))
+    do
+	#echo "bin drin"
+	#platzGroesse=$((${potentialEnd[$i]}-${potentialStart[$i]}))
+	echo "pE: ${potentialEnd[$i]} ;;; pS: ${potentialStart[$i]}"
+	for (( y=${potentialEnd[$i]}; y>=${potentialStart[$i]}; y-- ))
+	do
+	    platzGroesse=$(($platzGroesse+1))
+	done
+	#echo "Das ist platzGroesse: $platzGroesse"
+	if [[ $platzGroesse -ge $seekGroesse ]]; then
+	    differenz=$(($platzGroesse-seekGroesse))
+		#echo "Das ist differenz: $differenz"
+	    if [[ $differenz -lt $kleinsteDifferenz ]]; then
+		kleinsteDifferenz=$differenz
+		kleinsteDifferenzStelle=${potentialStart[$i]}
+		#echo ">>>: Das ist kDS: $kleinsteDifferenzStelle"
+		platzGroesse=0
+	    fi
+	fi
+    done
+
+    counter=0
+    for (( y=$kleinsteDifferenzStelle; $counter<$(($seekGroesse)); y++ ))
+    do
+        speicher[$y]=$(($seekGroesse))
+	counter=$((counter+1))
+    done
+    groesseEins[$kleinsteDifferenzStelle]=$(($seekGroesse)) #Größe an der Stelle im Array einfügen
+    nameEins[$kleinsteDifferenzStelle]=$seekName #Name an der Stelle im Array einfügen
+    counter=0
+
+    return 1 #Erfolgreich -> Raus    return 0 #Nicht erfolgreich -> Ciao!
+}
+
+
+#speicherplatz=${array_gesamte_groesse} #größe des Speichers
+#speicherplatz=10
+#unset name
+#unset groesse
+#unset speicher
+#unset potentialStart
+#unset potentialEnd
+
+#insertBestFit 10 "heyy" 2
+#insertBestFit $1 $2 $3
+echo "--------"
+echo "Speicher:"
+#unset $speicherout
+for (( i=0; i<$speicherplatz; i++ ))
+do
+    if [[ -z "${speicher[$i]}" ]]; then
+    $speicherout[$i]="X"
+    else
+    $speicherout[$i]=${speicher[$i]}
+    fi
+done
+echo ${speicher[*]}
+echo "--------"
+echo "--------"
+echo "Groesse:"
+echo ${groesseEins[*]}
+echo "++++++++"
+echo "++++++++"
+echo "Name:"
+echo ${nameEins[*]}
+
---
+++ hauptsteuerung.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+clear
+#Benutzer wird aufgefordert, eine Gesamtspeichergroesse zu waehlen
+echo "Bitte geben Sie eine Gesamtspeichergroesse an."
+#Speichergroesse wird eingelesen
+read -r speichergroesse
+	#Nachdem die Groesse eingelesen wurde wird ueberprueft, ob die Eingabe andere Zeichen ausser Zahlen enthaelt oder nicht groesser als 0 ist
+	#Wenn das der Fall ist, wird solange nach der Groesse gefragt, bis keine anderen Zeichen ausser Zahlen enthalten sind und die Eingabe groesser 0 ist
+	#Als Quelle fuer die Funktionsaufrufe ist simulationssteuerungFunktionen.sh definiert
+	#Der Rueckgabewert der Funktion keineZeichenGroesserNull ist die Gesamtspeichergroesse, welche in der Variablen gewaehlteGesamtspeichergroesse gespeichert wird
+	source ./simulationssteuerungFunktionen.sh
+	while [ -n "$(printf '%s\n' "$speichergroesse" | sed 's/[0-9]//g')" ] || [[ ! $speichergroesse -gt 0 ]]
+	do
+		keineZeichenNichtGroesserNull speichergroesse
+		zeichen speichergroesse
+		read -r speichergroesse
+	done
+	gewaehlteGesamtspeichergroesse=$(keineZeichenGroesserNull)
+echo "Ihre gewaehlte Speichergroesse ist: $gewaehlteGesamtspeichergroesse"
+#Array mit der gewaehlten Eingabe zur Groesse
+array_gesamte_groesse=($gewaehlteGesamtspeichergroesse)
+
+
+echo ""
+echo ""
+#Benutzer wird aufgefordert, das Realisierungskonzept auszuwaehlen, es gibt die Wahl zwischen den Shortcuts f,b,n,w,r und l
+echo "Bitte geben Sie ein, welches Realisierungskonzept genutzt werden soll. Es stehen folgenden Realisierungskonzepte zur Verfuegung:
+-> f (First Fit)
+-> b (Best Fit)
+-> n (Next Fit)
+-> w (Worst Fit)
+-> r (Random)
+-> l (Last Fit)"
+#Das ausgewaehlte Konzept wird eingelesen
+read -r konzept
+	#Als Quelle wird die Datei simulationssteuerungFunktionen.sh angegeben
+	#Es wird ueberprueft, ob die Eingabe einem der Realisierungskonzepte entspricht oder ob erneut nach einer Eingabe gefragt wird
+	source ./simulationssteuerungFunktionen.sh
+	while ! { [ "$konzept" = "f" ] || [ "$konzept" = "b" ] || [ "$konzept" = "n" ] || [ "$konzept" = "w" ] || [ "$konzept" = "r" ] || [ "$konzept" = "l" ]; }
+	do
+		ungueltigesRealisierungskonzept konzept
+		read -r konzept
+	done
+	#Die Eingabe entspricht einem der Realisierungskonzepte
+	#if-Abfragen fuer Realisierungskonzept zum ueberpruefen, welches Realisierungskonzept ausgewahlt wurde
+	#Die Funktionen werden aus der Quelle simulationssteuerungFunktionen.sh aufgerufen
+	source ./simulationssteuerungFunktionen.sh
+	#Es wurde das Konzept First Fit ausgewaehlt
+	if [ "$konzept" = "f" ]; then
+		echo "Ihr gewaehltes Konzept ist: First Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei firstfit.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertFirstFit
+ 		#Der Funktion werden dabei zwei Arrays uebergeben
+                #Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+				source ./firstfit.sh
+				insertFirstFit array_name_groesse array_gesamte_groesse
+                                abfrageAktion
+                                read -r befehl
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Best Fit ausgewaehlt
+	elif [ "$konzept" = "b" ]; then
+		echo "Ihr gewaehltes Konzept ist: Best Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei bestfit.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertBestFit
+		#Der Funktion werden dabei zwei Arrays uebergeben
+                #Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+				source ./bestfit.sh
+				echo "***********************"
+				echo "Speicher: $gewaehlteGesamtspeichergroesse"
+				echo "Name: $createPartitionsName"
+				echo "Groesse: $createPartitionsGroesse"
+				echo "************************"
+				insertBestFit $gewaehlteGesamtspeichergroesse $createPartitionsName $createPartitionsGroesse
+                                abfrageAktion
+                                read -r befehl
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Next Fit ausgewaehlt
+	elif [ "$konzept" = "n" ]; then
+		echo "Ihr gewaehltes Konzept ist: Next Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei nextfit.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertNextFit
+		#Der Funktion werden dabei zwei Arrays uebergeben
+                #Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+				source ./nextfit.sh
+				insertNextFit array_name_groesse array_gesamte_groesse
+                                abfrageAktion
+                                read -r befehl
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Worst Fit ausgewaehlt
+	elif [ "$konzept" = "w" ]; then
+		echo "Ihr gewaehltes Konzept ist: Worst Fit."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei worstfit.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertWorstFit
+		#Der Funktion werden dabei zwei Arrays uebergeben
+                #Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+				source ./worstfit.sh
+				insertWorstFit array_name_groesse array_gesamte_groesse
+                                abfrageAktion
+                                read -r befehl
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	#Es wurde das Konzept Random ausgewaehlt
+	elif [ "$konzept" = "r" ]; then
+		echo "Ihr gewaehltes Konzept ist: Random."
+		#Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+		abfrageAktion
+		#Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei random.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertRandom
+		#Der Funktion werden dabei zwei Arrays uebergeben
+		#Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+       		read -r befehl
+                	while [ "$befehl" != "q" ]
+                	do
+                		ablaufRealisierungskonzept befehl
+				source ./random.sh
+				insertRandom array_name_groesse array_gesamte_groesse
+				abfrageAktion
+				read -r befehl
+                	done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	elif [ "$konzept" = "l" ]; then
+                echo "Ihr gewaehltes Konzept ist: Last Fit."
+                #Die Funktion abfrageAktion wird von der angegebenen Quelle simulationssteuerungFunktionen.sh aufgerufen
+                abfrageAktion
+                #Der Befehl wird eingelesen
+		#Nachdem der jeweilige Befehl ausgefuehrt wurde (alle moeglich ausser q), wird auf die Datei lastfit.sh als Quelle verwiesen, wo die Eingaben weiter verarbeitet werden in der Funktion insertLastFit
+		#Der Funktion werden dabei zwei Arrays uebergeben
+                #Nachdem die Funktion ausgefuehrt wurde, wird erneut nach einer Eingabe gefragt
+                read -r befehl
+                        while [ "$befehl" != "q" ]
+                        do
+                                ablaufRealisierungskonzept befehl
+                                source ./lastfit.sh
+                                insertLastFit array_name_groesse array_gesamte_groesse
+                                abfrageAktion
+                                read -r befehl
+                        done
+                #Wenn der Befehl quit gewaehlt wurde, wird nun ausgegeben, dass der Simulator beendet wurde und im Hintergrund wird die Speicherbelegung zurueckgesetzt
+                #Funktion zum Zuruecksetzen des Speichers
+                echo "Der Simulator wurde beendet."
+	fi
